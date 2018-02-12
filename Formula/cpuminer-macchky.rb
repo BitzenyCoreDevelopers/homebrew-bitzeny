aa2214142fc3ffadb0ea0eb445d4cb76ce6171fc
onokatio <onokatio@gmail.com>
2018-02-12 21:25:14 +0900

    remove plist

--- Formula/cpuminer-macchky.rb
+++ Formula/cpuminer-macchky.rb
@@ -40,26 +40,6 @@ class CpuminerMacchky < Formula
     pkgshare.install "share/rpcuser"
   end
 
-  plist_options :manual => "bitzenyd"
-
-  def plist; <<~EOS
-    <?xml version="1.0" encoding="UTF-8"?>
-    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-    <plist version="1.0">
-    <dict>
-      <key>Label</key>
-      <string>#{plist_name}</string>
-      <key>ProgramArguments</key>
-      <array>
-        <string>#{opt_bin}/bitzenyd</string>
-      </array>
-      <key>RunAtLoad</key>
-      <true/>
-    </dict>
-    </plist>
-    EOS
-  end
-
   test do
     system "false"
   end
